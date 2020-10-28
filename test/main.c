078d1c9c08a4a7e55ee077b9475299914f14767c
josevelez08 <jose-0919@hotmail.com>
2020-10-28 04:51:16 +0000

    Test cases

---
+++ test/main.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+int main(void)
+{
+
+		_printf("%k\n");
+		return (0);
+}
---
+++ test/main1.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+int main(void)
+{
+
+		_printf("%d\n", INT_MIN);
+		_printf("%d\n",__INT_MAX__);
+		return (0);
+}
